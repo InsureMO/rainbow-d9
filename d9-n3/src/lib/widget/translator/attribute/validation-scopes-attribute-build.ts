6048970275d9b9a99faa206d242a789a1047be3d
brad.wu <brad.wu@ebaotech.com>
2023-10-30 19:39:36 +0800

    fixed: cannot read any attribute correctly
    since validation scope read all, add key matching for validation scope attribute read
    
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n3/src/lib/widget/translator/attribute/validation-scopes-attribute-build.ts
+++ d9-n3/src/lib/widget/translator/attribute/validation-scopes-attribute-build.ts
@@ -1,12 +1,13 @@
 import {NodeValidationScope, VUtils} from '@rainbow-d9/n1';
 import {ParsedListItemAttributePair} from '../../../semantic';
 import {Nullable, Undefinable} from '../../../utility-types';
+import {D9PropertyNames} from './attribute-name-util';
 import {AttributeValueBuild, WidgetPropertyName} from './types';
 
 export class ValidationScopesAttributeBuild implements AttributeValueBuild<Array<NodeValidationScope>> {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public accept(_key: WidgetPropertyName) {
-		return true;
+	public accept(key: WidgetPropertyName) {
+		return D9PropertyNames.VALIDATION_SCOPES === key;
 	}
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public build(value: Undefinable<string>, _list: ParsedListItemAttributePair): Nullable<Array<NodeValidationScope>> {
