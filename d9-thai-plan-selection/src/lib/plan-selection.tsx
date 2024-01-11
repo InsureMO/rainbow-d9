29916bdfadbbcf00312670814e76a95ffe1ddc1e
brad.wu <brad.wu@ebaotech.com>
2024-01-11 11:37:06 +0800

    feat: plan selection
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-thai-plan-selection/src/lib/plan-selection.tsx
+++ d9-thai-plan-selection/src/lib/plan-selection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {PlanSelectionProps} from './types';
 import {APlanSelection} from './widgets';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const PlanSelection = (props: PlanSelectionProps) => {
 	return <APlanSelection/>;
 };
