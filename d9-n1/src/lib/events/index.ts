5f7e6016d6ee47fea9deb712d66f32db81ca1f3c
brad.wu <brad.wu@ebaotech.com>
2024-01-11 11:25:21 +0800

    feat: add BridgeEventBus
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n1/src/lib/events/index.ts
+++ d9-n1/src/lib/events/index.ts
@@ -2,3 +2,4 @@ export * from './root-event-bus';
 export * from './container-event-bus';
 export * from './array-element-event-bus';
 export * from './wrapper-event-bus';
+export * from './bridge-event-bus';
