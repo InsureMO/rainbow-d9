64775cf46df03ac47da7ccabb2eb9fe6bd80b5c6
brad.wu <brad.wu@ebaotech.com>
2024-01-17 15:18:00 +0800

    feat: plan selection
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n3/src/lib/widget/translator/attribute/index.ts
+++ d9-n3/src/lib/widget/translator/attribute/index.ts
@@ -1,9 +1,16 @@
 export * from './types';
+export * from './constants';
 
 export * from './position-attribute-build';
 export * from './decorate-wrapper-build';
 export * from './validation-scopes-attribute-build';
+export * from './disablement-attribute-build';
+export * from './monitor-attribute-build';
+export * from './reaction-attribute-build';
+export * from './validation-attribute-build';
+export * from './visibility-attribute-build';
 export * from './any-attribute-build';
 
+export * from './snippet-attribute';
 export * from './attribute-utils';
 export * from './attribute-name-util';
--- d9-thai-plan-selection/src/lib/register.ts
+++ d9-thai-plan-selection/src/lib/register.ts
@@ -1,6 +1,68 @@
-import {NodeDef, registerWidget, VUtils} from '@rainbow-d9/n1';
-import {Widget} from '@rainbow-d9/n3';
+import {ExternalDefIndicator, NodeDef, registerWidget, Undefinable, VUtils} from '@rainbow-d9/n1';
+import {Semantic, Widget} from '@rainbow-d9/n3';
 import {PlanSelection} from './plan-selection';
+import {PlanSelectionDef} from './types';
+
+export const createAttributeBuild = <P extends keyof PlanSelectionDef, F = PlanSelectionDef[P]>(
+	attrName: P, createFunc: (parsed: string) => F
+): Widget.AttributeValueBuild<F | ExternalDefIndicator> => {
+	return {
+		accept: (key: Widget.WidgetPropertyName) => key === attrName,
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		build: (value: Undefinable<string>, list: Semantic.ParsedListItemAttributePair): Undefinable<F | ExternalDefIndicator> => {
+			const parsed = Widget.parseSnippet(value, list);
+			if (parsed instanceof ExternalDefIndicator) {
+				// in fact, external def indicator is already intercepted by caller,
+				// see AbstractTranslator.buildAttributeValue for more details
+				return parsed;
+			} else if (VUtils.isBlank(parsed)) {
+				return (void 0);
+			} else {
+				return createFunc(parsed);
+			}
+		}
+	};
+};
+export const PlanSelectionDefsBuild = createAttributeBuild<'defs'>(
+	'defs', (parsed: string) => {
+		return new Function(parsed) as PlanSelectionDef['defs'];
+	});
+export const PlanSelectionTitleBuild = createAttributeBuild<'planTitle'>(
+	'planTitle', (parsed: string) => {
+		return new Function('def', 'elementValueChanged', parsed) as PlanSelectionDef['planTitle'];
+	});
+export const PlanSelectionSubTitleBuild = createAttributeBuild<'planSubTitle'>(
+	'planSubTitle', (parsed: string) => {
+		return new Function('def', 'elementValueChanged', 'currencySymbol', 'premiumDescription', parsed) as PlanSelectionDef['planSubTitle'];
+	});
+export const PlanSelectionElementTitleBuild = createAttributeBuild<'elementTitle'>(
+	'elementTitle', (parsed: string) => {
+		return new Function('def', 'level', parsed) as PlanSelectionDef['elementTitle'];
+	});
+export const PlanSelectionElementFixedValueBuild = createAttributeBuild<'elementFixedValue'>(
+	'elementFixedValue', (parsed: string) => {
+		return new Function('options', parsed) as PlanSelectionDef['elementFixedValue'];
+	});
+export const PlanSelectionElementOptionsValueBuild = createAttributeBuild<'elementOptionsValue'>(
+	'elementOptionsValue', (parsed: string) => {
+		return new Function('options', parsed) as PlanSelectionDef['elementOptionsValue'];
+	});
+export const PlanSelectionElementNumberValueBuild = createAttributeBuild<'elementNumberValue'>(
+	'elementNumberValue', (parsed: string) => {
+		return new Function('options', parsed) as PlanSelectionDef['elementNumberValue'];
+	});
+export const PlanSelectionElementNumberValueValidatorBuild = createAttributeBuild<'elementNumberValueValidator'>(
+	'elementNumberValueValidator', (parsed: string) => {
+		return new Function('options', parsed) as PlanSelectionDef['elementNumberValueValidator'];
+	});
+export const PlanSelectionPlanOperatorsBuild = createAttributeBuild<'planOperators'>(
+	'planOperators', (parsed: string) => {
+		return new Function('def', 'plan', parsed) as PlanSelectionDef['planOperators'];
+	});
+export const PlanSelectionCalculateBuild = createAttributeBuild<'calculate'>(
+	'calculate', (parsed: string) => {
+		return new Function('event', parsed) as PlanSelectionDef['calculate'];
+	});
 
 export abstract class AbstractPlanSelectionTranslator extends Widget.SpecificWidgetTranslator<string> {
 	public beautifyProperties<Def extends NodeDef>(def: Partial<Def>): Def {
@@ -9,6 +71,18 @@ export abstract class AbstractPlanSelectionTranslator extends Widget.SpecificWid
 
 	public shouldWrapByFormCell(): boolean {
 		return false;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public getAttributeValueBuilders(): Array<Widget.AttributeValueBuild<any>> {
+		return [
+			PlanSelectionDefsBuild,
+			PlanSelectionTitleBuild, PlanSelectionSubTitleBuild, PlanSelectionElementTitleBuild,
+			PlanSelectionElementFixedValueBuild, PlanSelectionElementOptionsValueBuild,
+			PlanSelectionElementNumberValueBuild, PlanSelectionElementNumberValueValidatorBuild,
+			PlanSelectionPlanOperatorsBuild,
+			PlanSelectionCalculateBuild
+		];
 	}
 }
 
