c36d0a9f785eab22904c947ebb5412eef3b98d23
brad.wu <brad.wu@ebaotech.com>
2024-03-27 16:56:55 +0800

    checkboxes and radios fake as toggle buttons
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-sample-cra/src/demos/n2-basic-widgets/demo.md
+++ d9-sample-cra/src/demos/n2-basic-widgets/demo.md
@@ -1,14 +1,12 @@
 # Page::Demo Tab
 
-## Section::# 1. Basic Widgets
+## Section::# 1.1. Basic Widgets - Decorated Input
 
-- collapsible
-- marker: basic-widgets
 - DecoInput::Decorate Input::decorateInput
 	- label:
 		- labelOnValue
 		- property: decorateInput
-		- leads: Yes:
+		- leads: Masked:
 	- leads: Hello
 	- tails: World
 	- regex: abc$,^def; Starts with "def" or ends with "abc".
@@ -23,7 +21,7 @@
 	  ```
 - DecoNumber::Decorate Number Input::decorateNumberInput
 	- label:
-		- valueToLabel: `'Hello world, again.'`
+		- valueToLabel: `'Number, Grouping'`
 	- leads: $icons.check;$
 	- tails: %;$icons.caretLeft
 	- numeric
@@ -34,8 +32,9 @@
 	- tails: %;$icons.caretLeft
 	- numeric
 	- onKeyDown: @ext.keydown.numeric
-- Checkbox::A Checkbox::aCheckbox
-- Radio::A Radio::aRadio
+
+## Section::# 1.2. Basic Widgets - Dropdown
+
 - Dropdown::::dropdown
 	- label: Dropdown
 	- options:
@@ -51,9 +50,15 @@
 		- 3: A very very very very very very very very very very very very very very very very very very long Option #3
 		- 4: Option #4
 		- 5: Option #5
+
+## Section::# 1.3. Basic Widgets - Calendar
+
 - DateTime::Buddhist Era::buddhistEra
 	- dateFormat: BBBB/MM/DD
 	- timeFormat: HH
+
+## Section::# 1.4. Basic Widgets - Label
+
 - Label::A Label::label
 	- valueToLabel: `'Hello World'`
 	- leads: $icons.check;$
@@ -64,6 +69,10 @@
 - Caption::A Caption::caption
 	- $fc
 	- valueToLabel: `'Caption is [' + value + ']'`
+
+## Section::# 1.4. Basic Widgets - Radio
+
+- Radio::A Radio::aRadio
 - Radios::Radio Group::radios1
 	- options:
 		- 1: Option #1
@@ -73,6 +82,19 @@
 		- 5: A very very very very very very very very very very very very very very very very very very long Option #5
 	- place: 6
 	- columns: 4
+- Radios::Radio Group, Fake as Toggle Buttons::radios2
+	- data-as-toggle-button
+	- options:
+		- 1: Option #1
+		- 2: Option #2
+		- 3: Option #3
+		- 4: Option #4
+		- 5: Option #5
+	- place: 12
+
+## Section::# 1.5. Basic Widgets - Checkbox
+
+- Checkbox::A Checkbox::aCheckbox
 - Checks::Checkbox Group::checks1
 	- options:
 		- 1: Option #1
@@ -89,7 +111,19 @@
 		- 3: Option #3
 		- 4: Option #4
 	- single
+- Checks::Checkbox Group, Fake as Toggle Buttons::checks3
+	- data-as-toggle-button
+	- options:
+		- 1: Option #1
+		- 2: Option #2
+		- 3: Option #3
+		- 4: Option #4
+		- 5: Option #5
+	- place: 12
+
+## Section::# 1.6. Basic Widgets - Button
+
 - Button::
-	- text: Validate Inputs
+	- text: Validate Inputs, Scope "S1"
 	- click: validate s1
 
--- d9-sample-cra/src/demos/n2-basic-widgets/index.tsx
+++ d9-sample-cra/src/demos/n2-basic-widgets/index.tsx
@@ -1,19 +1,90 @@
 import {BaseModel, PropValue, StandaloneRoot} from '@rainbow-d9/n1';
-import {GlobalEventHandlers, GlobalRoot, ModelCarrier, OptionItems} from '@rainbow-d9/n2';
+import {CssVars, GlobalEventHandlers, GlobalRoot, ModelCarrier, OptionItems} from '@rainbow-d9/n2';
 import {KeyboardEvent} from 'react';
+import styled from 'styled-components';
 import {CustomEventHandler} from '../custom-event-handler';
 import {N2DemoDialogHandler} from '../n2-dialog-handler';
 import {useDemoMarkdown} from '../use-demo-markdown';
 import DemoData from './demo.json';
 import {markdown as DemoContent} from './demo.md';
 
+const StyleController = styled.div.attrs({})`
+    + div[data-w=d9-page] {
+        div[data-w=d9-radios][data-as-toggle-button] {
+            > span[data-w=d9-radios-option] {
+                border: ${CssVars.BORDER};
+                border-radius: ${CssVars.BORDER_RADIUS};
+                margin-left: 0;
+                margin-right: 0;
+                transition: color 0.3s, border-color 0.3s, background-color 0.3s, box-shadow 0.3s;
+
+                &:not(:first-child) {
+                    border-top-left-radius: 0;
+                    border-bottom-left-radius: 0;
+                    margin-left: -1px;
+                }
+
+                &:not(:last-child) {
+                    border-top-right-radius: 0;
+                    border-bottom-right-radius: 0;
+                }
+
+                &:hover {
+                    box-shadow: ${CssVars.HOVER_SHADOW};
+                }
+
+                &[data-checked=true] {
+                    color: ${CssVars.INVERT_COLOR};
+                    border-color: ${CssVars.PRIMARY_COLOR};
+                    background-color: ${CssVars.PRIMARY_COLOR};
+
+                    &:hover {
+                        box-shadow: ${CssVars.PRIMARY_HOVER_SHADOW};
+                    }
+                }
+
+                > div[data-w=d9-radio] {
+                    display: none;
+                }
+            }
+        }
+
+        div[data-w=d9-checkboxes][data-as-toggle-button] {
+            > span[data-w=d9-checkboxes-option] {
+                border: ${CssVars.BORDER};
+                border-radius: ${CssVars.BORDER_RADIUS};
+                margin-left: 0;
+                margin-right: 8px;
+                transition: color 0.3s, border-color 0.3s, background-color 0.3s, box-shadow 0.3s;
+
+                &:hover {
+                    box-shadow: ${CssVars.HOVER_SHADOW};
+                }
+
+                &[data-checked=true] {
+                    color: ${CssVars.INVERT_COLOR};
+                    border-color: ${CssVars.PRIMARY_COLOR};
+                    background-color: ${CssVars.PRIMARY_COLOR};
+
+                    &:hover {
+                        box-shadow: ${CssVars.PRIMARY_HOVER_SHADOW};
+                    }
+                }
+
+                > div[data-w=d9-checkbox] {
+                    display: none;
+                }
+            }
+        }
+    }
+`;
 export const N2BasicWidgets = () => {
 	const def = useDemoMarkdown(DemoContent);
 	const externalDefs = {
 		keydown: {
 			numeric: (event: KeyboardEvent<HTMLInputElement>) => {
 				console.log(`Key event[key=${event.key}, code=${event.code}] capture.`);
-				if (!['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(event.key)) {
+				if (event.key.length === 1 && !['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(event.key)) {
 					event.preventDefault();
 					return false;
 				}
@@ -30,6 +101,7 @@ export const N2BasicWidgets = () => {
 	return <GlobalRoot>
 		<CustomEventHandler/>
 		<N2DemoDialogHandler/>
+		<StyleController/>
 		<StandaloneRoot {...def} $root={DemoData} externalDefs={externalDefs}/>
 	</GlobalRoot>;
 };
