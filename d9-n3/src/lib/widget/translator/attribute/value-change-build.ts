af7b67d6f4a06ad492a3dac8c210e1083a0a27b8
brad.wu <brad.wu@ebaotech.com>
2024-04-02 20:24:20 +0800

    fixed: incorrect import
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n3/src/lib/widget/translator/attribute/value-change-build.ts
+++ d9-n3/src/lib/widget/translator/attribute/value-change-build.ts
@@ -1,4 +1,4 @@
-import {ValueChangeableNodeDef} from '@rainbow-d9/n1/src';
+import {ValueChangeableNodeDef} from '@rainbow-d9/n1';
 import {createAsyncSnippetBuild} from './snippet-attribute-build';
 
 export const ValueChangedBuild =
