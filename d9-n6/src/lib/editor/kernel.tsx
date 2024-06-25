6b6483f2a40fdd8d147a7a9182f6ef0854aaa096
brad.wu <brad.wu@ebaotech.com>
2024-06-25 17:52:44 +0800

    feat: n6
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n6/src/lib/editor/kernel.tsx
+++ d9-n6/src/lib/editor/kernel.tsx
@@ -1,10 +1,10 @@
 import {CanvasWidget} from '@projectstorm/react-canvas-core';
-import createEngine, {DiagramModel} from '@projectstorm/react-diagrams';
+import createEngine, {DiagramModel, NodeModel} from '@projectstorm/react-diagrams';
 import {DiagramEngine} from '@projectstorm/react-diagrams-core';
 import {useForceUpdate, useThrottler, VUtils} from '@rainbow-d9/n1';
 import React, {useEffect, useRef} from 'react';
 import {FileDef, FileDefDeserializer, FileDefSerializer} from '../definition';
-import {initEngine} from '../diagram';
+import {initEngine, NextStepPortModel, StartNodeModel} from '../diagram';
 import {Labels} from '../labels';
 import {PlaygroundEventTypes, usePlaygroundEventBus} from '../playground-event-bus';
 import {EditorProps, MarkdownContent} from '../types';
@@ -62,6 +62,70 @@ const createDiagramHandlers = (options: {
 		}
 	};
 
+};
+
+interface GridCell {
+	node?: NodeModel;
+	x: number;
+	y: number;
+	maxWidth: number;
+	maxHeight: number;
+	top: number;
+	left: number;
+}
+
+const buildGrid = (previous: NodeModel, grid: Array<Array<GridCell>>, x: number, y: number) => {
+	// TODO compute sub step nodes
+	// compute next step node
+	const port = previous.getPort(NextStepPortModel.NAME);
+	if (port != null) {
+		const links = port.getLinks();
+		const link = Object.values(links)[0];
+		const next = link.getTargetPort().getNode();
+		grid[x] = grid[x] ?? [];
+		grid[x][y] = {
+			node: next, x: next.getPosition().x, y: next.getPosition().y,
+			maxWidth: -1, maxHeight: -1, top: -1, left: -1
+		};
+		buildGrid(next, grid, x, y + 1);
+	}
+};
+
+const computeGrid = (grid: Array<Array<GridCell>>, top: number, left: number, rowGap: number, columnGap: number) => {
+	let offsetX = left;
+	let offsetY = top;
+	const maxX = grid.length - 1;
+	const maxY = grid.reduce((max, column) => Math.max(max, column.length - 1), 0);
+	for (let x = 0; x <= maxX; x++) {
+		const column = grid[x];
+		new Array(maxY + 1).fill(1).forEach(y => {
+			if (column[y] == null) {
+				column[y] = {x, y, maxWidth: -1, maxHeight: -1, top: -1, left: -1};
+			}
+		});
+		const maxWidth = column.reduce((max, cell) => Math.max(max, cell.node?.width ?? 0), 0);
+		offsetX = offsetX + (x === 0 ? 0 : (grid[x - 1][0].maxWidth + columnGap));
+		column.forEach(cell => {
+			cell.maxWidth = maxWidth;
+			cell.left = cell.node == null ? (offsetX + maxWidth / 2) : (offsetX + (maxWidth - cell.node.width) / 2);
+		});
+	}
+	for (let y = 0; y <= maxY; y++) {
+		const row = grid.map(column => column[y]);
+		const maxHeight = row.reduce((max, cell) => Math.max(max, cell.node?.height ?? 0), 0);
+		offsetY = offsetY + (y === 0 ? 0 : (grid[0][y - 1].maxHeight + columnGap));
+		row.forEach(cell => {
+			cell.maxHeight = maxHeight;
+			cell.top = cell.node == null ? (offsetY + maxHeight / 2) : (offsetY + (maxHeight - cell.node.height) / 2);
+		});
+	}
+	grid.forEach(column => {
+		column.forEach(cell => {
+			if (cell.node != null) {
+				cell.node.setPosition(cell.left, cell.top);
+			}
+		});
+	});
 };
 
 export const EditorKernel = (props: EditorProps) => {
@@ -140,7 +204,22 @@ export const EditorKernel = (props: EditorProps) => {
 		if (EditorKernelDiagramStatus.FIRST_PAINT !== stateRef.current.diagramStatus) {
 			return;
 		}
-		// TODO re-calculate node positions
+
+		// re-calculate node positions
+		const grid: Array<Array<GridCell>> = [];
+		const nodes = stateRef.current.engine.getModel().getNodes();
+		const startNode = nodes.find(node => node instanceof StartNodeModel);
+		grid[0] = grid[0] ?? [];
+		grid[0][0] = {
+			node: startNode, x: startNode.getPosition().x, y: startNode.getPosition().y,
+			maxWidth: -1, maxHeight: -1, top: -1, left: -1
+		};
+		buildGrid(startNode, grid, 0, 1);
+		computeGrid(grid, 64, 64, 64, 64);
+		// must reset model, otherwise links might not be repositioned, don't know why.
+		const model = new DiagramModel();
+		model.addAll(...stateRef.current.engine.getModel().getModels());
+		stateRef.current.engine.setModel(model);
 		stateRef.current.diagramStatus = EditorKernelDiagramStatus.IN_SERVICE;
 		forceUpdate();
 	}, [forceUpdate, stateRef.current.diagramStatus]);
