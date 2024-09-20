c2494d43f978caa41948c56a3cfa9ab1431ab377
brad.wu <brad.wu@ebaotech.com>
2024-09-21 00:26:51 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- create-rainbow-d9-app/templates/src/utils/env-utils.ts
+++ create-rainbow-d9-app/templates/src/utils/env-utils.ts
@@ -1,6 +1,6 @@
 export const getAppName = () => import.meta.env.VITE_APP_TITLE || 'Frontend';
 export const isThemeEnabled = () => import.meta.env.VITE_THEME_ENABLED === 'true';
-export const getDefaultThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_CODE || 'light';
+export const getDefaultThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_CODE;
 export const getDefaultLightThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_LIGHT || 'light';
 export const getDefaultDarkThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_DARK || 'dark';
 
--- d9-n99/src/utils/storage-utils.ts
+++ d9-n99/src/utils/storage-utils.ts
@@ -1,3 +1,4 @@
-const SIDE_MENU_FOLD_KEY = 'sideMenuFold';
+const STORGE_PREFIX = 'd9-n99-';
+const SIDE_MENU_FOLD_KEY = `${STORGE_PREFIX}side-menu-fold`;
 export const isSideMenuFold = () => localStorage.getItem(SIDE_MENU_FOLD_KEY) === 'true';
 export const setSideMenuFold = (fold: boolean) => localStorage.setItem(SIDE_MENU_FOLD_KEY, fold ? 'true' : 'false');
