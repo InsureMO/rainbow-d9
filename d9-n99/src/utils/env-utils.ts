a08f7f8b81c7554028952faf9969f8e8a583f3f7
brad.wu <brad.wu@ebaotech.com>
2024-09-21 00:17:41 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n99/src/utils/env-utils.ts
+++ d9-n99/src/utils/env-utils.ts
@@ -1,4 +1,4 @@
-export const getAppName = () => import.meta.env.VITE_APP_TITLE || 'D9 Frontend';
+export const getAppName = () => import.meta.env.VITE_APP_TITLE || 'Frontend';
 export const isThemeEnabled = () => import.meta.env.VITE_THEME_ENABLED === 'true';
 export const getDefaultThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_CODE || 'light';
 export const getDefaultLightThemeCode = () => import.meta.env.VITE_THEME_DEFAULT_LIGHT || 'light';
@@ -9,6 +9,6 @@ export const getDefaultLangCode = () => import.meta.env.VITE_I18N_DEFAULT_CODE |
 
 export const isSideMenuEnabled = () => import.meta.env.VITE_DISABLE_SIDE_MENU !== 'true';
 export const isBannerEnabled = () => import.meta.env.VITE_DISABLE_BANNER !== 'true';
-export const isThemeSwitcherEnabled = () => import.meta.env.VITE_DISABLE_THEME_SWITCHER !== 'true';
-export const isI18NSwitcherEnabled = () => import.meta.env.VITE_DISABLE_I18N_SWITCHER !== 'true';
+export const isThemeSwitcherEnabled = () => isThemeEnabled() && import.meta.env.VITE_DISABLE_THEME_SWITCHER !== 'true';
+export const isI18NSwitcherEnabled = () => isI18NEnabled() && import.meta.env.VITE_DISABLE_I18N_SWITCHER !== 'true';
 export const isUserProfileEnabled = () => import.meta.env.VITE_DISABLE_USER_PROFILE !== 'true';
