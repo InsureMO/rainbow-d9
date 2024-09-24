8257a900ef30ec1c383c30f508e3c7fbc0193dbe
brad.wu <brad.wu@ebaotech.com>
2024-09-25 00:21:27 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n99/src/global-settings/menu.tsx
+++ d9-n99/src/global-settings/menu.tsx
@@ -27,31 +27,39 @@ export interface AppMenuItem extends AppMenu {
 	type: AppMenuType.ITEM;
 }
 
+export enum PrebuiltAppMenuCode {
+	PREFERENCES = 'preferences',
+	LANGUAGES = 'languages',
+	THEMES = 'theme',
+	SYSTEM_THEME = 'system-theme'
+}
+
 /**
  * available themes, using in theme switcher
  */
 export const askMenus = (): Array<AppMenuGroup | AppMenuItem> => {
 	return [
+		// do not change the codes of the items, the language and theme switcher will use them
 		{
-			code: 'preferences', type: AppMenuType.GROUP,
+			code: PrebuiltAppMenuCode.PREFERENCES, type: AppMenuType.GROUP,
 			icon: <SettingsIcon/>, text: <IntlLabel keys={['menus.preferences']} value="Preferences"/>,
 			items: [
 				{
-					code: 'language', type: AppMenuType.GROUP,
+					code: PrebuiltAppMenuCode.LANGUAGES, type: AppMenuType.GROUP,
 					icon: <LanguageIcon/>, text: <IntlLabel keys={['menus.language']} value="Language"/>,
 					items: askAvailableLanguages().map(lang => {
 						return {code: lang.code, type: AppMenuType.ITEM, icon: lang.icon, text: lang.text};
 					})
 				},
 				{
-					code: 'theme', type: AppMenuType.GROUP,
+					code: PrebuiltAppMenuCode.THEMES, type: AppMenuType.GROUP,
 					icon: <ThemeIcon/>, text: <IntlLabel keys={['menus.theme']} value="Color Theme"/>,
 					items: [
 						...askAvailableThemes().map<AppMenuItem>(theme => {
 							return {code: theme.code, type: AppMenuType.ITEM, icon: theme.icon, text: theme.text};
 						}),
 						{
-							code: 'system', type: AppMenuType.ITEM,
+							code: PrebuiltAppMenuCode.SYSTEM_THEME, type: AppMenuType.ITEM,
 							icon: <SystemThemeIcon/>, text: <IntlLabel keys={['theme.system']} value="System"/>
 						}
 					]
