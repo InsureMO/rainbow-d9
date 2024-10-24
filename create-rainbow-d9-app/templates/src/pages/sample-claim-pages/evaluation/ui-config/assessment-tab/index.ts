f2f40a7f318f7e1b217df40964d9243568a1e30c
brad.wu <brad.wu@ebaotech.com>
2024-10-24 16:10:52 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- create-rainbow-d9-app/templates/src/pages/sample-claim-pages/evaluation/ui-config/assessment-tab/index.ts
+++ create-rainbow-d9-app/templates/src/pages/sample-claim-pages/evaluation/ui-config/assessment-tab/index.ts
@@ -1,12 +1,23 @@
+import {markdown as productAdjustmentFactorTable} from './adjustment-factor-table.d9';
 import {markdown as assessmentTab} from './assessment-tab.d9';
-import {markdown as policyPaymentSummaryAdjustmentItemTable} from './policy-payment-summary-adjustment-item-table.d9';
-import {markdown as policyPaymentSummaryProductTable} from './policy-payment-summary-product-table.d9';
+import {markdown as productLiabilityEvaluationTable} from './liability-evaluation-table.d9';
+import {markdown as policyAdjustmentItemTable} from './policy-adjustment-item-table.d9';
 import {markdown as policyPaymentSummarySection} from './policy-payment-summary.d9';
+import {markdown as productAdjustmentItemTable} from './product-adjustment-item-table.d9';
+import {markdown as productBasicSection} from './product-basic-section.d9';
+import {markdown as productTable} from './product-table.d9';
 
+const createProductTable = () => {
+	return productTable
+		.replace('\t- Box::$$basic-section', productBasicSection.split('\n').map(line => `\t${line}`).join('\n'))
+		.replace('\t- Box::$$adjustment-factor-table', productAdjustmentFactorTable.split('\n').map(line => `\t${line}`).join('\n'))
+		.replace('\t- Box::$$liability-evaluation-table', productLiabilityEvaluationTable.split('\n').map(line => `\t${line}`).join('\n'))
+		.replace('\t- Box::$$adjustment-item-table', productAdjustmentItemTable.split('\n').map(line => `\t${line}`).join('\n'));
+};
 const createPaymentSummarySection = () => {
 	return policyPaymentSummarySection
-		.replace('- Box::$$products-table', policyPaymentSummaryProductTable)
-		.replace('- Box::$$adjustment-items-table', policyPaymentSummaryAdjustmentItemTable);
+		.replace('- Box::$$products-table', createProductTable())
+		.replace('- Box::$$adjustment-items-table', policyAdjustmentItemTable);
 };
 
 export const createAssessmentTab = () => {
