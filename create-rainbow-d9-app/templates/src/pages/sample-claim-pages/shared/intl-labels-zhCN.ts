1cf548d131034122eb3f2dec18dd9f2553b502f6
brad.wu <brad.wu@ebaotech.com>
2024-10-25 09:51:39 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- create-rainbow-d9-app/templates/src/pages/sample-claim-pages/shared/intl-labels-zhCN.ts
+++ create-rainbow-d9-app/templates/src/pages/sample-claim-pages/shared/intl-labels-zhCN.ts
@@ -424,5 +424,6 @@ export const zhCNIntlLabels = redressByCutPrefix({
 	'claim.action.reminder': 'Reminder',
 	'claim.action.withdraw': 'Withdraw',
 	'claim.action.comment': 'Comment',
-	'claim.action.reload-policy': 'Reload Policy'
+	'claim.action.reload-policy': 'Reload Policy',
+	'claim.action.page-navigator': 'Navigator in this Page'
 }, 'claim');
