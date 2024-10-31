3d1a1ac1742a112c6a14acce75c6917de449d180
brad.wu <brad.wu@ebaotech.com>
2024-10-31 23:13:26 +0800

    npx
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- create-rainbow-d9-app/templates/src/bootstrap/global-styles.tsx
+++ create-rainbow-d9-app/templates/src/bootstrap/global-styles.tsx
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components';
+import {createGlobalStyle, css} from 'styled-components';
 import '../assets/fonts.css';
 import {createThemeStyles} from '../global-settings';
 
@@ -32,6 +32,56 @@ export const AppGlobalStyles = (() => {
             box-sizing: border-box;
         }
 
-        ${createThemeStyles()}
+        ${createThemeStyles().map(([tag, n2, app]) => {
+            // noinspection CssUnresolvedCustomProperty,CssNoGenericFontName
+            return css`
+                div${tag} ~ * {
+                    ${n2};
+                    font-family: var(--d9-font-family);
+                    font-size: var(--d9-font-size);
+                    color: var(--d9-font-color);
+                    // div[data-h-scroll],
+                    // div[data-v-scroll] {
+                    // 	&::-webkit-scrollbar {
+                    // 		background-color: transparent;
+                    // 		height: var(--d9-scroll-height);
+                    // 		width: var(--d9-scroll-width)
+                    // 	}
+                    // 	&::-webkit-scrollbar-track {
+                    // 		background-color: var(--d9-scroll-track-color);
+                    // 		border-radius: var(--d9-scroll-border-radius);
+                    // 	}
+                    // 	&::-webkit-scrollbar-thumb {
+                    // 		background-color: var(--d9-scroll-thumb-color);
+                    // 		border-radius: var(--d9-scroll-border-radius);
+                    // 	}
+                    // }
+
+                    svg[data-icon=dark-theme] {
+                        transform: scale(0.85);
+                        transform-origin: center;
+                    }
+
+                    svg[data-icon=system-theme] {
+                        transform: scale(0.9);
+                        transform-origin: center;
+                    }
+
+                    svg[data-icon=theme] {
+                        transform: scale(0.9) translateY(-2px);
+                        transform-origin: center;
+                    }
+
+                    svg[data-icon=search] {
+                        transform: scale(0.8);
+                        transform-origin: center;
+                    }
+                }
+
+                div${tag} ~ div[data-w=app-frame] {
+                    ${app};
+                }
+            `;
+        })}
 	`;
 })();
