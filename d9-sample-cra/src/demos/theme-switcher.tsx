9982e2ca8af40d6b5e16b826b281c19361501144
brad.wu <brad.wu@ebaotech.com>
2024-11-11 23:03:47 +0800

    remove theme switcher since it only present switching for editors, not whole page
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-sample-cra/src/demos/theme-switcher.tsx
+++ d9-sample-cra/src/demos/theme-switcher.tsx
@@ -1,16 +1,18 @@
-import {BridgeToRootEventTypes, useBridgeEventBus} from '@rainbow-d9/n1';
-import {ButtonBarAlignment, UnwrappedButton, UnwrappedButtonBar} from '@rainbow-d9/n2';
+// import {BridgeToRootEventTypes, useBridgeEventBus} from '@rainbow-d9/n1';
+// import {ButtonBarAlignment, UnwrappedButton, UnwrappedButtonBar} from '@rainbow-d9/n2';
+import {Fragment} from 'react';
 
 export const ThemeSwitcher = () => {
-	const {fire} = useBridgeEventBus();
-	const onLightClicked = () => fire(BridgeToRootEventTypes.THEME_CHANGED, 'light');
-	const onDarkClicked = () => fire(BridgeToRootEventTypes.THEME_CHANGED, 'dark');
+	// const {fire} = useBridgeEventBus();
+	// const onLightClicked = () => fire(BridgeToRootEventTypes.THEME_CHANGED, 'light');
+	// const onDarkClicked = () => fire(BridgeToRootEventTypes.THEME_CHANGED, 'dark');
 
-	// @ts-ignore
-	return <UnwrappedButtonBar alignment={ButtonBarAlignment.CENTER}>
-		{/** @ts-ignore */}
-		<UnwrappedButton onClick={onLightClicked}>Light</UnwrappedButton>
-		{/** @ts-ignore */}
-		<UnwrappedButton onClick={onDarkClicked}>Dark</UnwrappedButton>
-	</UnwrappedButtonBar>;
+	return <Fragment/>;
+	// // @ts-ignore
+	// return <UnwrappedButtonBar alignment={ButtonBarAlignment.CENTER}>
+	// 	{/** @ts-ignore */}
+	// 	<UnwrappedButton onClick={onLightClicked}>Light</UnwrappedButton>
+	// 	{/** @ts-ignore */}
+	// 	<UnwrappedButton onClick={onDarkClicked}>Dark</UnwrappedButton>
+	// </UnwrappedButtonBar>;
 };
