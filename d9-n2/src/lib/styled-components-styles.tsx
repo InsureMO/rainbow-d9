4f6af68714e21a491a2dd8cf47219d20a98c380f
brad.wu <brad.wu@ebaotech.com>
2024-11-20 11:29:57 +0800

    omit error message of attach @data- attribute to dom node
    Signed-off-by: brad.wu <brad.wu@ebaotech.com>

--- d9-n2/src/lib/styled-components-styles.tsx
+++ d9-n2/src/lib/styled-components-styles.tsx
@@ -16,17 +16,30 @@ export type WSDP<T> = WithDataPrefix<T>
 
 export type StyleSheetManagerProps = Omit<IStyleSheetManager, 'shouldForwardProp'> & {
 	ignoreRegexps?: Array<RegExp>;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	ignoreRules?: Array<(...data: any[]) => boolean>;
 };
 export const StyleSheetManager = (props: StyleSheetManagerProps) => {
 	const [shouldForwardProp] = useState(() => {
 		const ignoredRegexps = props.ignoreRegexps ?? [
 			/^Warning: React does not recognize the `[^`]+` prop on a DOM element\./,
-			/^Warning: Received `%s` for a non-boolean attribute `%s`./
+			/^Warning: Received `%s` for a non-boolean attribute `%s`./,
+			/^Warning: Invalid values? for props? %s on <%s> tag./,
+			/^Warning: Invalid attribute name: `%s`%s/
+		];
+		const ignoreRules = [
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(...data: any[]) => ignoredRegexps.some(ignore => ignore.test(data[0])),
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(...data: any[]) => /^Warning: Invalid values? for props? %s on <%s> tag./.test(data[0]) && data[1]?.includes('@data-'),
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(...data: any[]) => /^Warning: Invalid attribute name: `%s`%s/.test(data[0]) && data[1].includes('@data-'),
+			...(props.ignoreRules ?? [])
 		];
 		const error = console.error;
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		console.error = function (...data: any[]) {
-			if (data != null && data[0] != null && typeof data[0] === 'string' && ignoredRegexps.some(ignore => ignore.test(data[0]))) {
+			if (data != null && data[0] != null && typeof data[0] === 'string' && ignoreRules.some(rule => rule(...data))) {
 				return console.debug(...data);
 			} else {
 				return error(...data);
